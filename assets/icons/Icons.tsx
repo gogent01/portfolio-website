25411cadcc8cf0e4a9b2386f60d4c6a6f225a959
George <gogent01@gmail.com>
2023-12-22 10:31:52 +0100

    Tweak icon dimensions to fit design principles

--- assets/icons/Icons.tsx
+++ assets/icons/Icons.tsx
@@ -15,7 +15,7 @@ const GithubIcon = React.forwardRef(function GithubIcon(
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 496 512"
+        viewBox="-8 0 512 512"
         fill="currentColor"
         style={{ transform: 'translate3d(0, 0, 0)' }}
       >
@@ -39,7 +39,7 @@ const LinkedInIcon = React.forwardRef(function LinkedInIcon(
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
+        viewBox="-32 0 512 512"
         fill="currentColor"
         style={{ transform: 'translate3d(0, 0, 0)' }}
       >
@@ -63,7 +63,7 @@ const MailIcon = React.forwardRef(function MailIcon(
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
-        viewBox="0 0 512 512"
+        viewBox="-32 -32 576 576"
         style={{ transform: 'translate3d(0, 0, 0)' }}
       >
         <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
@@ -86,7 +86,7 @@ const CvIcon = React.forwardRef(function CvIcon(
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
-        viewBox="0 0 512 512"
+        viewBox="0 -32 576 576"
         style={{ transform: 'translate3d(0, 0, 0)' }}
       >
         <path d="M0 64C0 28.7 28.7 0 64 0h160v128c0 17.7 14.3 32 32 32h128v288c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0l128 128z" />
