c0062671a1a9ea6a8fd180d6ee5f9d702caf9882
George <gogent01@gmail.com>
2024-08-31 17:18:35 +0200

    Add cards for the first part of "Code Complete" book

--- content/cards/code-complete/foundation/metadata.tsx
+++ content/cards/code-complete/foundation/metadata.tsx
@@ -4,13 +4,17 @@ import React from 'react';
 import { Deck } from '@/types';
 
 const DECK_INFO = React.cache((): Deck => {
-  const deckPath = path.join(process.cwd(), '/content/cards/css/typography');
+  const deckPath = path.join(
+    process.cwd(),
+    '/content/cards/code-complete/foundation'
+  );
 
   return {
     sectionKey: path.basename(path.dirname(deckPath)),
     key: path.basename(deckPath),
-    title: 'Typography',
-    description: 'Learn about typography in CSS.',
+    title: 'Code Construction Foundations',
+    description:
+      'Key takeaways from the book "Code Complete" by Steve McConnell, part I. What are the prerequisites for building a solid project? Covers problem definition, gathering of requirements, system architecture.',
     cardsPath: deckPath,
     cardKeys: fs
       .readdirSync(deckPath)
--- content/cards/metadata.tsx
+++ content/cards/metadata.tsx
@@ -1,4 +1,8 @@
 import { Deck } from '@/types';
-import { DECK_INFO } from '@/content/cards/css/typography/metadata';
+import TYPOGRAPHY_META from '@/content/cards/css/typography/metadata';
+import CODE_COMPLETE_FOUNDATION_META from '@/content/cards/code-complete/foundation/metadata';
 
-export const ALL_DECKS: Deck[] = [DECK_INFO()];
+export const ALL_DECKS: Deck[] = [
+  TYPOGRAPHY_META(),
+  CODE_COMPLETE_FOUNDATION_META(),
+];
