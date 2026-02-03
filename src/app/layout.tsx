e8a5f010f44a49fd77d35fb987f3085a4be59b2b
Georgy Mishurovskiy <gamishurovskiy@gmail.com>
2026-02-03 18:37:10 +0100

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 
 import { Red_Hat_Text } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const mainFont = Red_Hat_Text({ subsets: ['latin'] });
 
@@ -18,6 +19,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body className={mainFont.className}>
         <main>{children}</main>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MJV84TY2L9"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MJV84TY2L9');
+          `}
+        </Script>
       </body>
     </html>
   );
